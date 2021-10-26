6ac01200e19eb92b544e5f780d6567e26b5dbf6a
e.marquesdev <e.marquesdev@gmail.com>
2021-10-26 16:10:49 -0300

    Implementação: Salvando o tempo de estudo

--- data.js
+++ data.js
@@ -1,17 +1,31 @@
 const jsonFile = require('jsonfile-promised');
 const fs = require('fs');
+const jsonfilePromised = require('jsonfile-promised');
 
 module.exports = {
     salvaDados(nomeCurso, tempoEstudado){
         let arquivoDoCurso = __dirname + '/data/' + nomeCurso + '.json';
         if(fs.existsSync(arquivoDoCurso)){
-
+            this.adicionaTempoAoCurso(arquivoDoCurso,tempoEstudado);
         }else{
             this.criaArquivoDeCurso(arquivoDoCurso,{})
                 .then(()=>{
-
+                    this.adicionaTempoAoCurso(arquivoDoCurso,tempoEstudado);
                 })
         }
+    },
+    adicionaTempoAoCurso(arquivoDoCurso, tempoEstudado){
+        let dados = {
+            ultimoEstudo: new Date().toString(),
+            tempo: tempoEstudado
+        }
+
+        jsonfilePromised.writeFile(arquivoDoCurso,dados,{spaces:2})
+            .then(()=>{
+                console.log('Tempo salvo com sucesso')
+            }).catch(()=>{
+                console.log(err)
+            })
     },
     criaArquivoDeCurso(arquivoDoCurso, conteudoArquivo){
        return jsonFile.writeFile(arquivoDoCurso, conteudoArquivo)
